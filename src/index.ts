efd30fb7c0f587e197f7c4c14c88ee845f11e4fd
leehaoze <leehaoze@outlook.com>
2021-09-09 23:38:43 +0800

    修改文字坐标定位方式

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,37 @@
 import ChildProcess from "child_process"
 import Path from "path"
 import "./better-console"
+import addText from "./interceptor/method/topic/topicapi";
+import fs from "fs";
 
-function createBot() {
-    const botProcess = ChildProcess.fork(Path.join(__dirname, "./bot"), process.argv)
-    botProcess.on("exit", function (code: number) {
-        console.log("机器人进程已退出，退出码：" + code)
-        if (code !== 0) {
-            console.log("正在重新启动……")
-            createBot()
-        }
-    })
-    botProcess.on("error", function (e) {
-        console.log("机器人进程发生错误：" + e)
-        console.log("正在重新启动……")
-        createBot()
-    })
-}
-createBot()
+// function createBot() {
+//     const botProcess = ChildProcess.fork(Path.join(__dirname, "./bot"), process.argv)
+//     botProcess.on("exit", function (code: number) {
+//         console.log("机器人进程已退出，退出码：" + code)
+//         if (code !== 0) {
+//             console.log("正在重新启动……")
+//             createBot()
+//         }
+//     })
+//     botProcess.on("error", function (e) {
+//         console.log("机器人进程发生错误：" + e)
+//         console.log("正在重新启动……")
+//         createBot()
+//     })
+// }
+// createBot()
+const basePicture = "./img/bg.png";
+let stockpool = JSON.parse(fs.readFileSync('./stocks.json', 'utf-8'))
+let top_num = 1234
+addText(
+    basePicture,
+    {
+        fontSize: 30,
+        color: 'red',
+        left: 270,
+        top: top_num
+        //1340-1250 = 90
+    },
+    './img/bg1.png',
+    stockpool
+);
